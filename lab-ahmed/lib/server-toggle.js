1b8ca68a3b5ecfd482d506fe18f96d59316f33ca
Ahmed Ali <Ahmed98148@gmail.com>
2018-03-11 21:22:55 -0700

    fisnished test and fixed linter errors

--- lab-ahmed/lib/server-toggle.js
+++ lab-ahmed/lib/server-toggle.js
@@ -1,22 +1,26 @@
 'use strict';
 
-const debug = require('debug')('cfgram: server-toggle');
-const mongoos = require('mongoose');
+const debug = require('debug')('cfgram:server-toggle');
+const mongoose = require('mongoose');
 
-exports.serverOn = function(server, done){
-  if(!server.isRunning){
+module.exports = exports = {};
+
+exports.serverOn = function(server, done) {
+  if (!server.isRunning) {
     server.listen(process.env.PORT, () => {
-
-    })
+      server.isRunning = true;
+      debug('server up!');
+      done();
+    });
     return;
   }
   done();
-}
+};
 
-exports.serverOff = function(server, done){
-  if(server.isRunning){
-    server.close(err =>{
-      if(err) return done(err);
+exports.serverOff = function(server, done) {
+  if (server.isRunning) {
+    server.close( err => {
+      if (err) return done(err);
       server.isRunning = false;
       mongoose.connection.close();
       debug('server down!');
@@ -25,4 +29,4 @@ exports.serverOff = function(server, done){
     return;
   }
   done();
-}
+};
--- lab-ahmed/model/user.js
+++ lab-ahmed/model/user.js
@@ -9,64 +9,67 @@ const Promise = require('bluebird');
 const debug = require('debug')('cfgram:user');
 
 const Schema = mongoose.Schema;
+
 const userSchema = Schema({
-  username: {type: String, required: true, unique: true},
-  email: {type: String, requried: true, unique: true},
-  password: {type: String, required: true},
-  findHash: {type: String, unique: true},
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  findHash: { type: String, unique: true },
 });
 
 userSchema.methods.generatePasswordHash = function(password) {
   debug('generatePasswordHash');
+
   return new Promise((resolve, reject) => {
-    brypt.hash(password, 10, (err, hash) => {
+    bcrypt.hash(password, 10, (err, hash) => {
       if (err) return reject(err);
+      this.password = hash;
       resolve(this);
     });
   });
-}
+};
 
-userSchema.methods.comparePasswordHash = function(password){
-  debug('comparePassowrdHash');
+userSchema.methods.comparePasswordHash = function(password) {
+  debug('comparePasswordHash');
 
   return new Promise((resolve, reject) => {
     bcrypt.compare(password, this.password, (err, valid) => {
       if (err) return reject(err);
-      if (!valid) return reject(createError(401, 'invalid passord'));
-      reslove(this);
+      if (!valid) return reject(createError(401, 'invalid password'));
+      resolve(this);
     });
   });
-}
+};
 
-userSchema.methods.generateFindHash = function(){
+userSchema.methods.generateFindHash = function() {
   debug('generateFindHash');
+
   return new Promise((resolve, reject) => {
     let tries = 0;
 
-    _generateFIndHash.call(this);
+    _generateFindHash.call(this);
 
-    function _generateFIndHash(){
+    function _generateFindHash() {
       this.findHash = crypto.randomBytes(32).toString('hex');
       this.save()
-      .then(() => resolve(this.findHash))
-      .catch(err => {
-        if (tries > 3) return reject(err);
-        tries++;
-        _generateFIndHash.call(this);
-      })
+        .then( () => resolve(this.findHash))
+        .catch( err => {
+          if (tries > 3) return reject(err);
+          tries++;
+          _generateFindHash.call(this);
+        });
     }
-  })
-}
+  });
+};
 
-userSchema.methods.generateToken = function(){
-  debug('generateToken')
+userSchema.methods.generateToken = function() {
+  debug('generateToken');
 
   return new Promise((resolve, reject) => {
     this.generateFindHash()
-    .then(findHash => resolve(jwt.sign({token: findHash}, process.env.APP_SECERET)))
-    .catch(err => reject(err));
-  })
+      .then( findHash => resolve(jwt.sign({ token: findHash }, process.env.APP_SECRET)))
+      .catch( err => reject(err));
+  });
 };
 
 module.exports = mongoose.model('user', userSchema);
-
